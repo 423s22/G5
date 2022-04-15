e6fe070e514ebfd23f3f09f4612307bb982ffbdc
johnjubes <john.n.jubes@gmail.com>
2022-04-15 11:11:10 -0600

    changed name of functions

--- theme-app-extension/assets/app-block.js
+++ theme-app-extension/assets/app-block.js
@@ -1,40 +1,44 @@
 
 
-function addAllItems(array) {
+function removeItemFromCart(id) {
 
-  var newVariantIdArray = array.split(',');
+  const val = id.toString();
 
+  var newVariantIdArray = new Array(val);
 
   var product_data = newVariantIdArray.map(variantId => {
-    return {quantity: 1, id: variantId}
-  })
+      return {quantity: 1, id: variantId}
+    })
+  
+  
+    var data = {
+      items: product_data
+    }
+          
+    fetch('/cart/add.js', {
+      body: JSON.stringify(data),
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With':'xmlhttprequest'
+      },
+      method: 'POST'
+    }).then((response) => {
+      return response.json();
+    }).then((json) => {
+      /* yay! our products were added - do something here to indicate to the user */
+      console.log('products', json)
+      window.location.reload();
+    }).catch((err) => {
+      /* uh oh, we have error. */
+      console.error(err)
+    });
 
-
-  var data = {
-    items: product_data
-  }
-        
-  fetch('/cart/add.js', {
-    body: JSON.stringify(data),
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Requested-With':'xmlhttprequest'
-    },
-    method: 'POST'
-  }).then((response) => {
-    return response.json();
-  }).then((json) => {
-    /* yay! our products were added - do something here to indicate to the user */
-    console.log('products', json)
-    window.location.reload();
-  }).catch((err) => {
-    /* uh oh, we have error. */
-    console.error(err)
-  });
+    
+  
 }
 
-function addAllItemsfromv(id) {
+function addItemToCart(id) {
 
     const val = id.toString();
 
